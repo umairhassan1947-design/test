a60a9f7dc9d32127e72d46d4b58c6f0969f121c5
umairhassan1947-design <umairhassan1947@gmail.com>
2025-08-21 16:49:01 +0500

    loops

---
+++ scripts/loops.py
@@ -0,0 +1,23 @@
+#learning loops
+primenum = [2,3,5,7]
+for x in primenum:
+    print(x) #will print all primenum in the list
+
+for z in range(3,100,10):
+    print(z) #prints 3 and takes steps of 10 and prints till 100
+
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >=10 :
+        break #loop breaks after this
+
+    
+
+
+
+
+
+
+
