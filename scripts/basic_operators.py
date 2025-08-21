c8e8524eb9a896bf74dacaf2d78441921102022f
umairhassan1947-design <umairhassan1947@gmail.com>
2025-08-21 12:07:42 +0500

    understanding basic operators in python

---
+++ scripts/basic_operators.py
@@ -0,0 +1,12 @@
+#learning basic operators
+n = 1+2*3/4
+print(n) #just as any other language we can use operators for addition subtraction multiplication division
+remainder = 6%5
+print(remainder) # % gives the remainder
+square = 5**2
+print(square) #using 2 * makes power relationship likewise we can make cube as well
+name= 'umair'+' '+'hassan'
+print(name) #using addition operator can cocatenate strings
+
+
+
